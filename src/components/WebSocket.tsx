f8ecdac26196f8e71bf6ee4e5fe3215bb9604611
ayu <umaminokiami@gmail.com>
2024-10-30 13:47:10 +0900

    add

--- src/components/WebSocket.tsx
+++ src/components/WebSocket.tsx
@@ -11,8 +11,8 @@ const WebSocket = () => {
   // const name = searchParams.get("name") || "";
   const roomId = searchParams.get("roomId") || "";
 
-  const url = `ws://127.0.0.1:8000/ws/${roomId}`;
-  // const url = `wss://hartlink-api.onrender.com/ws/${roomId}`;
+  // const url = `ws://127.0.0.1:8000/ws/${roomId}`;
+  const url = `wss://hartlink-api.onrender.com/ws/${roomId}`;
 
   const [heartRate, setheartRate] = useState<HeartRate>();
   // const [heartRate, setheartRate] = useState<string>(" ");
